530184eb4dfbdd55defa907f1f8f5d5b96199ab9
CD Clark III <clifton.clark@gmail.com>
2020-08-29 13:57:38 -0500

    houskeeping: update version number in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'fspathtree',
   packages = ['fspathtree'],
-  version = '0.3',
+  version = '0.4',
   license='MIT',
   description = 'A small utility for wrapping trees (nested dict/list) that allows filesystem-like path access, including walking up with "../".',
   author = 'CD Clark III',
