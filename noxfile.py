03e636ad721fbc00352198aff9b6d015a986dba6
CD Clark III <clifton.clark@gmail.com>
2024-07-08 21:39:05 -0500

    chore: setting up nox

--- noxfile.py
+++ noxfile.py
@@ -1,26 +1,6 @@
 """Nox sessions."""
-import os
-import shlex
-import shutil
-import sys
-from pathlib import Path
-from textwrap import dedent
 
 import nox
-
-
-try:
-    from nox_poetry import Session
-    from nox_poetry import session
-except ImportError:
-    message = f"""\
-    Nox failed to import the 'nox-poetry' package.
-
-    Please install it using the following command:
-
-    {sys.executable} -m pip install nox-poetry"""
-    raise SystemExit(dedent(message)) from None
-
 
 package = "fspathtree/"
 python_versions = ["3.10"]
@@ -28,9 +8,18 @@ nox.needs_version = ">= 2021.6.6"
 nox.options.sessions = ("tests",)
 
 
-@session(python=python_versions)
-def tests(session: Session) -> None:
+@nox.session(python=python_versions)
+def tests(session: nox.Session) -> None:
     """Run the test suite."""
     session.install(".")
     session.install("pytest")
     session.run("pytest", *session.posargs)
+
+
+@nox.session
+def lint(session: nox.Session) -> None:
+    """Run th elinter"""
+    session.install("pre-commit")
+    session.run(
+        "pre-commit", "run", "--all-file", "--show-diff-on-failure", *session.posargs
+    )
