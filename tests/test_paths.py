aea1c0d85b35b42d66ecc0289205283cc697f556
CD Clark III <clifton.clark@gmail.com>
2024-07-08 21:56:20 -0500

    style: ran black formatter

--- tests/test_paths.py
+++ tests/test_paths.py
@@ -3,15 +3,20 @@ import pathlib
 import copy
 from fspathtree import fspathtree
 
+
 def test_get_nodes_path():
-  d = {'one' : 1, 'level1' : {'two' : 2, 'nums' : [1,2,3], 'level3' : {'three' : 3}}, 'nums' : [1,2,3] }
-  t = fspathtree(d)
+    d = {
+        "one": 1,
+        "level1": {"two": 2, "nums": [1, 2, 3], "level3": {"three": 3}},
+        "nums": [1, 2, 3],
+    }
+    t = fspathtree(d)
 
-  x = t['/level1/level3']
+    x = t["/level1/level3"]
 
-  assert type(x.path()) == pathlib.PurePosixPath
-  assert str(x.path()) == "/level1/level3"
-  assert x.path() == pathlib.PurePosixPath("/level1/level3")
+    assert type(x.path()) == pathlib.PurePosixPath
+    assert str(x.path()) == "/level1/level3"
+    assert x.path() == pathlib.PurePosixPath("/level1/level3")
 
 
 def test_path_normalization():
